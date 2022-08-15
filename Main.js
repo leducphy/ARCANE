db67e7dd82a8efb7020212d9f5de62c28b525917
Phi Le <91577321+leducphiz@users.noreply.github.com>
2022-08-15 20:40:13 +0700

    a

--- Main.js
+++ Main.js
@@ -2,6 +2,7 @@ const buyTk = document.querySelectorAll('.js-buy-tk')
 const modal = document.querySelector('.js-modal')
 const close = document.querySelector('.js-modal-close')
 
+
 function showTk() {
     modal.classList.add('opend')
 }
@@ -11,7 +12,38 @@ function hideTk() {
 }
 
 for (const i of buyTk) {
-    i.addEventListener('click', showTk  )
+    i.addEventListener('click', showTk)
 }
 
 close.addEventListener('click', hideTk)
+
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight;
+
+// đóng mở menu khi click ra ngoài
+mobileMenu.onclick = function () {
+    var isClose = header.clientHeight === headerHeight;
+    if (isClose) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+
+//đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.onclick = function (event) {
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if (isParentMenu) {
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+
+        }
+
+    }
+}
